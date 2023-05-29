a01f12fb5976ba0c7de5512ec0ff626d84a52d21
AltGitWork <altgitwork@gmail.com>
2023-05-29 21:21:01 +0300

    edit acrd and add card in category page

--- src/pages/homepage/Home.js
+++ src/pages/homepage/Home.js
@@ -2,13 +2,13 @@ import './Home.scss';
 import Head from '../../components/pagehead/Head';
 import MostPopular from '../../components/homemostpoopular/MostPopular';
 import SmallAboutUs from '../../components/smallaboutussection/SmallAboutUs';
-
+import {Cards} from "../../Cards"
 const Home = () => {
     
     return(
         <>
             <Head/>
-            <MostPopular/>
+            <MostPopular title={"Most Popular"} cards={Cards} number={8}/>
             <SmallAboutUs/>
         </>
     )
